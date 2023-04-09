ba3b43f9f7c54e582ebed0d5359c3ac511450df0
ichDan <somevildhjarta@gmail.com>
2023-04-09 16:05:07 +0300

    Initial commit

---
+++ src/main/java/com/example/testtaskdc/TestTaskDcApplication.java
@@ -0,0 +1,14 @@
+package com.example.testtaskdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskDcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskDcApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/testtaskdc/TestTaskDcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtaskdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskDcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
